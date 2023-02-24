da4399f4885f731bebda1998984d85336bfaf037
maxim.budnik <maxim@lassie.co>
2023-02-24 15:12:17 +0200

    skill tag form

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -42,6 +42,7 @@ export const HomePage: FC = (props) => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+
   return (
     <>
       <Modal title="Report a bug" open={isModalOpen} footer={[]} onCancel={handleCancel}>
@@ -86,8 +87,6 @@ export const HomePage: FC = (props) => {
             <ActivityCard title={"Manage deliveries"} link={EditorRoutes.manageDeliveries} icon={"🚚"} />
             <ActivityCard title={"Manage projects"} link={EditorRoutes.manageProjects} icon={"🚧"} />
             <ActivityCard title={"Manage skill tags"} link={EditorRoutes.manageSkillTags} icon={"🤹"} />
-
-
           </Space>
         </>}
         {isAdmin && <>
