1aaa1f8cbf36398ece10c9f195859499b959933a
maxim.budnik <maxim@lassie.co>
2023-03-14 19:55:12 +0200

    update nginx config

--- server/src/middlewares/allowCrossDomain.middleware.ts
+++ server/src/middlewares/allowCrossDomain.middleware.ts
@@ -2,7 +2,7 @@ import { ORIGIN } from "@config";
 
 export const allowCrossDomain = (req, res, next) => {
   res.header(`Access-Control-Allow-Origin`, ORIGIN);
-  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,PATCH,DELETE`);
+  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,PATCH,DELETE,OPTIONS`);
   res.header(`Access-Control-Allow-Headers`, `Content-Type,Authorization`);
   next();
 };
