5e3824b2dcf4f2478cff60ad9b2908dbb2cdf8d1
maxim.budnik <maxim@lassie.co>
2023-07-31 18:08:02 +0300

    rebuild app

--- client/src/wrappers/RequireEditor.tsx
+++ client/src/wrappers/RequireEditor.tsx
@@ -1,20 +1,20 @@
-import React, {FC} from 'react';
-import {useAuth0} from '@auth0/auth0-react';
-import {Outlet, useNavigate} from "react-router-dom";
-import {AppRoutes} from "../router/AppRoutes";
-import {UserRole} from "../shared/user.interface";
+import React, { FC } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { AppRoutes } from "../router/AppRoutes";
+import { UserRole } from "../shared/user.interface";
 
 export const useIsEditor = () => {
-    const {user,} = useAuth0();
-    return user?.role === UserRole.admin || user?.role === UserRole.editor
-}
+  const { user } = useAuth0();
+  return user?.role === UserRole.admin || user?.role === UserRole.editor;
+};
 const RequireEditor: FC = () => {
-    const isEditor = useIsEditor()
-    const navigate = useNavigate()
-    if (!isEditor) {
-        navigate(AppRoutes.index);
-        return null
-    }
-    return <Outlet/>;
+  const isEditor = useIsEditor();
+  const navigate = useNavigate();
+  if (!isEditor) {
+    navigate(AppRoutes.index);
+    return null;
+  }
+  return <Outlet />;
 };
 export default RequireEditor;
