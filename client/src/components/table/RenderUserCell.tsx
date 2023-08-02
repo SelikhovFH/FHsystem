b3906c5ad6cac3e8bb1c019dff2a1f8d14c8284d
SelikhovFH <hlib.selikhov@fh.solutions>
2023-08-03 01:16:45 +0200

    test fix

--- client/src/components/table/RenderUserCell.tsx
+++ client/src/components/table/RenderUserCell.tsx
@@ -4,25 +4,33 @@ import { theme, Typography } from "antd";
 import { Link as NavLink } from "react-router-dom";
 import { getUserProfileRoute } from "../../router/AppRoutes";
 
-const { Link, Text, Title } = Typography;
+const { Link, Title } = Typography;
 
-
-export const UserCell: FC<{ user: User, title?: boolean }> = ({ user, title }) => {
+export const UserCell: FC<{ user: User; title?: boolean }> = ({
+  user,
+  title,
+}) => {
   const userName = `${user.name} ${user.surname}`;
   const { token } = theme.useToken();
   return (
     <NavLink to={getUserProfileRoute(user._id)}>
       <Link>
-        {title ? <Title style={{ color: token.colorLink }} level={3}>{userName}</Title> : <>
-          {userName}
+        {title ? (
+          <Title style={{ color: token.colorLink }} level={3}>
+            {userName}
+          </Title>
+        ) : (
+          <>
+            {userName}
 
-          <div style={{ fontSize: "0.75em", whiteSpace: "nowrap" }}>({user.email})</div>
-        </>}
-
+            <div style={{ fontSize: "0.75em", whiteSpace: "nowrap" }}>
+              ({user.email})
+            </div>
+          </>
+        )}
       </Link>
     </NavLink>
   );
-
 };
 
 export const renderUserCell = (user?: User): ReactNode => {
@@ -36,7 +44,18 @@ export const renderMultipleUsersCell = (users?: User[]): ReactNode => {
   if (!users) {
     return null;
   }
-  return <div style={{ display: "flex", flexDirection: "row", gap: 16, flexWrap: "wrap" }}>
-    {users.map(u => <UserCell user={u} key={u._id} />)}
-  </div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: 16,
+        flexWrap: "wrap",
+      }}
+    >
+      {users?.map((u) => (
+        <UserCell user={u} key={u._id} />
+      ))}
+    </div>
+  );
 };
--- client/src/pages/editor/ManageUsersPage.tsx
+++ client/src/pages/editor/ManageUsersPage.tsx
@@ -11,7 +11,7 @@ import {
   Space,
   Table,
   Tag,
-  Typography
+  Typography,
 } from "antd";
 import { FC, useEffect, useState } from "react";
 import { Gutter } from "../../components/Gutter";
@@ -19,7 +19,12 @@ import * as yup from "yup";
 import { getYupRule } from "../../utils/yupRule";
 import { ErrorsBlock } from "../../components/ErrorsBlock";
 import { AppHeader } from "../../layouts/Header";
-import { SalaryRecord, User, UserRole, UserStatus } from "../../shared/user.interface";
+import {
+  SalaryRecord,
+  User,
+  UserRole,
+  UserStatus,
+} from "../../shared/user.interface";
 import { capitalize } from "../../utils/strings";
 import { useApiFactory } from "../../services/apiFactory";
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
@@ -44,7 +49,7 @@ const registerSchema = yup.object().shape({
   email: yup.string().required().email(),
   role: yup.string().required(),
   name: yup.string().required(),
-  surname: yup.string().required()
+  surname: yup.string().required(),
 });
 
 const updateSchema = registerSchema.shape({
@@ -54,127 +59,195 @@ const updateSchema = registerSchema.shape({
   emergencyContact: yup.string(),
   location: yup.string().required(),
   title: yup.string().required(),
-  salaryHistory: yup.array().of(yup.object().shape({
-    value: yup.number().required(),
-    date: yup.string().required()
-  })).required(),
+  salaryHistory: yup
+    .array()
+    .of(
+      yup.object().shape({
+        value: yup.number().required(),
+        date: yup.string().required(),
+      })
+    )
+    .required(),
   cvLink: yup.string(),
   status: yup.string().required(),
-  skills: yup.array()
+  skills: yup.array(),
 });
 
-const RegisterForm: FC<Omit<FormProps, "initialValues" | "buttonText">> = ({ form, onFinish, buttonDisabled }) => {
+const RegisterForm: FC<Omit<FormProps, "initialValues" | "buttonText">> = ({
+  form,
+  onFinish,
+  buttonDisabled,
+}) => {
   useEffect(() => {
     form.resetFields();
   }, []);
-  return <Form className={styles.form} form={form} name="registerUser"
-               layout={"vertical"}
-               onFinish={onFinish}
-               autoComplete="off">
-    <Form.Item rules={[getYupRule(registerSchema)]} label="Name"
-               name="name">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(registerSchema)]} label="Surname"
-               name="surname">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(registerSchema)]} label="Email"
-               name="email">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(registerSchema)]} label="User role"
-               name="role">
-      <Select
-        options={Object.values(UserRole).map(v => ({ value: v, label: UserRolesLabels[v] }))}
-      />
-    </Form.Item>
-    <Form.Item>
-      <Button disabled={buttonDisabled} type="primary" htmlType="submit">
-        Register new employee
-      </Button>
-    </Form.Item>
-  </Form>;
+  return (
+    <Form
+      className={styles.form}
+      form={form}
+      name="registerUser"
+      layout={"vertical"}
+      onFinish={onFinish}
+      autoComplete="off"
+    >
+      <Form.Item rules={[getYupRule(registerSchema)]} label="Name" name="name">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(registerSchema)]}
+        label="Surname"
+        name="surname"
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(registerSchema)]}
+        label="Email"
+        name="email"
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(registerSchema)]}
+        label="User role"
+        name="role"
+      >
+        <Select
+          options={Object.values(UserRole).map((v) => ({
+            value: v,
+            label: UserRolesLabels[v],
+          }))}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button disabled={buttonDisabled} type="primary" htmlType="submit">
+          Register new employee
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
-const UpdateForm: FC<Omit<FormProps, "buttonText">> = ({ form, onFinish, buttonDisabled, initialValues }) => {
+const UpdateForm: FC<Omit<FormProps, "buttonText">> = ({
+  form,
+  onFinish,
+  buttonDisabled,
+  initialValues,
+}) => {
   useEffect(() => {
     form.resetFields();
   }, [initialValues]);
-  return <Form className={styles.form} form={form} name="updateForm"
-               initialValues={initialValues}
-               layout={"vertical"}
-               onFinish={onFinish}
-               autoComplete="off">
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Email"
-               name="email">
-      <Input disabled />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="User role"
-               name="role">
-      <Select
-        disabled
-        options={Object.values(UserRole).map(v => ({ value: v, label: UserRolesLabels[v] }))}
-      />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Name"
-               name="name">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Surname"
-               name="surname">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Work start date"
-               name="workStartDate">
-      <DatePicker />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Birth date"
-               name="birthDate">
-      <DatePicker />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Phone"
-               name="phone">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Emergency contact"
-               name="emergencyContact">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Location (time zone)"
-               name="location">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Title"
-               name="title">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Skills"
-               name="skills">
-      <SkillTagsSelect />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Salary"
-               name="salaryHistory">
-      <SalaryInput />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="CV link"
-               name="cvLink">
-      <Input />
-    </Form.Item>
-    <Form.Item rules={[getYupRule(updateSchema)]} label="Status"
-               name="status">
-      <Select
-        options={Object.values(UserStatus).map(v => ({ value: v, label: capitalize(v) }))}
-      />
-    </Form.Item>
-    <Form.Item>
-      <Button disabled={buttonDisabled} type="primary" htmlType="submit">
-        Update employee
-      </Button>
-    </Form.Item>
-  </Form>;
+  return (
+    <Form
+      className={styles.form}
+      form={form}
+      name="updateForm"
+      initialValues={initialValues}
+      layout={"vertical"}
+      onFinish={onFinish}
+      autoComplete="off"
+    >
+      <Form.Item rules={[getYupRule(updateSchema)]} label="Email" name="email">
+        <Input disabled />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="User role"
+        name="role"
+      >
+        <Select
+          disabled
+          options={Object.values(UserRole).map((v) => ({
+            value: v,
+            label: UserRolesLabels[v],
+          }))}
+        />
+      </Form.Item>
+      <Form.Item rules={[getYupRule(updateSchema)]} label="Name" name="name">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="Surname"
+        name="surname"
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="Work start date"
+        name="workStartDate"
+      >
+        <DatePicker />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="Birth date"
+        name="birthDate"
+      >
+        <DatePicker />
+      </Form.Item>
+      <Form.Item rules={[getYupRule(updateSchema)]} label="Phone" name="phone">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="Emergency contact"
+        name="emergencyContact"
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="Location (time zone)"
+        name="location"
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item rules={[getYupRule(updateSchema)]} label="Title" name="title">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="Skills"
+        name="skills"
+      >
+        <SkillTagsSelect />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="Salary"
+        name="salaryHistory"
+      >
+        <SalaryInput />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="CV link"
+        name="cvLink"
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        rules={[getYupRule(updateSchema)]}
+        label="Status"
+        name="status"
+      >
+        <Select
+          options={Object.values(UserStatus).map((v) => ({
+            value: v,
+            label: capitalize(v),
+          }))}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button disabled={buttonDisabled} type="primary" htmlType="submit">
+          Update employee
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
-
 
 export const ManageUsersPage: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -187,21 +260,21 @@ export const ManageUsersPage: FC = () => {
     addMutation,
     deleteMutation,
     editMutation,
-    messageContext
+    messageContext,
   } = useApiFactory<User[], Partial<User>>({
     basePath: "/users",
     add: {
       path: "/users/register",
       onSuccess: () => {
         setIsOpen(false);
-      }
+      },
     },
     edit: {
       onSuccess: () => {
         setIsOpen(false);
         setUserToEdit(null);
-      }
-    }
+      },
+    },
   });
 
   const showAddModal = () => {
@@ -219,15 +292,18 @@ export const ManageUsersPage: FC = () => {
     setIsOpen(false);
   };
 
-
   const onAddFinish = (values: any) => {
     addMutation.mutate(values);
   };
 
   const onEditFinish = (_values: User) => {
     const { auth0id, role, email, ...values } = _values;
+    editMutation.mutate({
+      ...values,
+      _id: userToEdit?._id,
     // @ts-ignore
-    editMutation.mutate({ ...values, _id: userToEdit?._id, skills: _values.skills.map(skill => skill._id) });
+      skills: _values.skills.map((skill) => skill._id),
+    });
   };
 
   const onDelete = (values: any) => {
@@ -235,8 +311,13 @@ export const ManageUsersPage: FC = () => {
   };
 
   const onEditClick = (user: User) => {
+    setUserToEdit({
+      ...user,
     // @ts-ignore
-    setUserToEdit({ ...user, workStartDate: dayjs(user.workStartDate), birthDate: dayjs(user.birthDate) });
+      workStartDate: dayjs(user.workStartDate),
+    // @ts-ignore
+      birthDate: dayjs(user.birthDate),
+    });
     setIsOpen(true);
   };
 
@@ -256,134 +337,173 @@ export const ManageUsersPage: FC = () => {
       render: (_, record) => {
         return renderUserCell(record);
       },
-      fixed: "left"
+      fixed: "left",
     },
     {
       title: "Role",
       dataIndex: "role",
       key: "role",
       render: (role: UserRole) => {
-        return <span style={{ whiteSpace: "nowrap" }}>
-          {UserRolesLabels[role]}
-        </span>;
-      }
+        return (
+          <span style={{ whiteSpace: "nowrap" }}>{UserRolesLabels[role]}</span>
+        );
+      },
     },
     {
       title: "Title",
       dataIndex: "title",
       key: "title",
       render: (text) => {
-        return <span style={{ whiteSpace: "nowrap" }}>
-          {text}
-        </span>;
-      }
+        return <span style={{ whiteSpace: "nowrap" }}>{text}</span>;
+      },
     },
     {
       title: "Status",
       dataIndex: "status",
       key: "status",
       render: (status: UserStatus) => {
-        return <span style={{ whiteSpace: "nowrap" }}>
-          {UserStatusLabels[status]}
-        </span>;
-      }
+        return (
+          <span style={{ whiteSpace: "nowrap" }}>
+            {UserStatusLabels[status]}
+          </span>
+        );
+      },
     },
     {
       title: "Phone",
       dataIndex: "phone",
-      key: "phone"
+      key: "phone",
     },
     {
       title: "Emergency contact",
       dataIndex: "emergencyContact",
-      key: "emergencyContact"
+      key: "emergencyContact",
     },
     {
       title: "Work start date",
       dataIndex: "workStartDate",
       key: "workStartDate",
-      render: renderDateCell
+      render: renderDateCell,
     },
     {
       title: "Birth date",
       dataIndex: "birthDate",
       key: "birthDate",
-      render: renderDateCell
+      render: renderDateCell,
     },
     {
       title: "Location (time zone)",
       dataIndex: "location",
-      key: "location"
+      key: "location",
     },
     {
       title: "Skills",
       dataIndex: "skills",
       key: "skills",
       render: (values: SkillTag[]) => {
-        return <Space style={{ maxWidth: 500, minWidth: 300 }} wrap>
-          {values.map((v, idx) => <Tag color={SkillTagCategoryToColor[v.category]}
-                                       key={idx}>{`${v.category} | ${v.name}`}</Tag>)}
-        </Space>;
-      }
+        return (
+          <Space style={{ maxWidth: 500, minWidth: 300 }} wrap>
+            {values?.map((v, idx) => (
+              <Tag
+                color={SkillTagCategoryToColor[v.category]}
+                key={idx}
+              >{`${v.category} | ${v.name}`}</Tag>
+            ))}
+          </Space>
+        );
+      },
     },
     {
       title: "Salary history",
       dataIndex: "salaryHistory",
       key: "salaryHistory",
       render: (values: SalaryRecord[]) => {
-        return <Space style={{ maxWidth: 500, minWidth: 300 }} wrap>
-          {values.map((v, idx) => <Tag key={idx}>{formatMoney(v.value)} | {formatDate(v.date)}</Tag>)}
-        </Space>;
-      }
+        return (
+          <Space style={{ maxWidth: 500, minWidth: 300 }} wrap>
+            {values.map((v, idx) => (
+              <Tag key={idx}>
+                {formatMoney(v.value)} | {formatDate(v.date)}
+              </Tag>
+            ))}
+          </Space>
+        );
+      },
     },
     {
       title: "CV link",
       dataIndex: "cvLink",
       key: "cvLink",
       render: (link: string) => {
-        return <a href={link} target={"_blank"} style={{ whiteSpace: "nowrap" }}>Check resume</a>;
-      }
+        return (
+          <a
+            href={link}
+            target={"_blank"}
+            style={{ whiteSpace: "nowrap" }}
+            rel="noreferrer"
+          >
+            Check resume
+          </a>
+        );
+      },
     },
     {
       title: "Operations",
       dataIndex: "operations",
       key: "operations",
       render: (_, record) => {
-        return <Space>
-          <Button onClick={() => onEditClick(record)} type={"primary"} icon={<EditOutlined />}>
-          </Button>
-          <Button disabled={deleteMutation.isLoading || !isAdmin} onClick={() => onDelete(record)} danger
-                  type={"primary"}
-                  icon={<DeleteOutlined />}>
-          </Button>
-
-        </Space>;
+        return (
+          <Space>
+            <Button
+              onClick={() => onEditClick(record)}
+              type={"primary"}
+              icon={<EditOutlined />}
+            />
+            <Button
+              disabled={deleteMutation.isLoading || !isAdmin}
+              onClick={() => onDelete(record)}
+              danger
+              type={"primary"}
+              icon={<DeleteOutlined />}
+            />
+          </Space>
+        );
       },
-      fixed: "right"
-    }
-
+      fixed: "right",
+    },
   ];
 
   return (
     <>
       {messageContext}
-      {isOpen && !!userToEdit && <Modal footer={[]} title={"Update employee"} open={true}
-                                        onCancel={handleEditCancel}>
-        <UpdateForm
-          initialValues={userToEdit}
-          form={form}
-          onFinish={onEditFinish}
-          buttonDisabled={editMutation.isLoading}
-        />
-      </Modal>}
-      {isOpen && !userToEdit && <Modal footer={[]} title={"Register employee"} open={true}
-                                       onCancel={handleAddCancel}>
-        <RegisterForm
-          form={form}
-          onFinish={onAddFinish}
-          buttonDisabled={addMutation.isLoading}
-        />
-      </Modal>}
+      {isOpen && !!userToEdit && (
+        <Modal
+          footer={[]}
+          title={"Update employee"}
+          open={true}
+          onCancel={handleEditCancel}
+        >
+          <UpdateForm
+            initialValues={userToEdit}
+            form={form}
+            onFinish={onEditFinish}
+            buttonDisabled={editMutation.isLoading}
+          />
+        </Modal>
+      )}
+      {isOpen && !userToEdit && (
+        <Modal
+          footer={[]}
+          title={"Register employee"}
+          open={true}
+          onCancel={handleAddCancel}
+        >
+          <RegisterForm
+            form={form}
+            onFinish={onAddFinish}
+            buttonDisabled={addMutation.isLoading}
+          />
+        </Modal>
+      )}
       <AppHeader title={"Manage employees"} />
       <Content style={{ margin: 32 }}>
         <ErrorsBlock errors={[users.error, addMutation.error]} />
@@ -394,11 +514,23 @@ export const ManageUsersPage: FC = () => {
             Register
           </Button>
         </Card>
-        {addMutation.status === "success" &&
-          <Alert style={{ marginTop: 16 }} type={"info"} message={"Success!"}
-                 description={<Paragraph>Password change link was sent to user</Paragraph>} />}
+        {addMutation.status === "success" && (
+          <Alert
+            style={{ marginTop: 16 }}
+            type={"info"}
+            message={"Success!"}
+            description={
+              <Paragraph>Password change link was sent to user</Paragraph>
+            }
+          />
+        )}
         <Gutter size={2} />
-        <Table scroll={{ x: true }} loading={users.isLoading} dataSource={users.data} columns={columns} />
+        <Table
+          scroll={{ x: true }}
+          loading={users.isLoading}
+          dataSource={users.data}
+          columns={columns}
+        />
       </Content>
     </>
   );
